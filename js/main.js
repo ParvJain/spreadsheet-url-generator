2506728d1da94a887c72718699102944285f6be5
Parv Jain <parv.wutizup@gmail.com>
2016-08-27 18:48:41 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var currentKey = document.getElementById("city").value;
+
+function cityChange() {
+	currentKey = document.getElementById("city").value;
+}
+
+function buildUrl() {
+	var searchQuery = document.getElementById("activityString").value;	
+	a = "https://spreadsheets.google.com/tq?tqx=out:html&"
+		+ "tq=SELECT B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V WHERE lower(B) CONTAINS '" + searchQuery.toLowerCase() + "'"
+		+ "&key=" + currentKey;
+	a = encodeURI(a);
+	console.log(a);
+	document.getElementById("link").textContent=a;	
+}
+
