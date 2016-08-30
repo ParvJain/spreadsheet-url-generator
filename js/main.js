0be2c8e533609a95552a5eb396f65eb4476efed4
Parv Jain <parv.wutizup@gmail.com>
2016-08-30 16:20:00 +0530

    form fixed

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ function cityChange() {
 	currentKey = document.getElementById("city").value;
 }
 
+// function redirect(url) {
+//
+// }
+
 function buildUrl() {
 	var searchQuery = document.getElementById("activityString").value;
 	a = "https://spreadsheets.google.com/tq?tqx=out:html&"
 		+ "tq=SELECT B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V WHERE lower(B) CONTAINS '" + searchQuery.toLowerCase() + "'"
 		+ "&key=" + currentKey;
 	a = encodeURI(a);
-	console.log(a);
-	document.location =a;	
+	location.href = a;
+	return false;
 }
